49c4df243d472d8ab8cdc81b29ee7812fc95db28
Ted Dowd <tdowd@hyperionhomes.com>
2016-08-24 20:35:24 -0500

    take in vars from cli
    will allow any number of names to be drawn by passing them in as args

--- nameOutofaHat.py
+++ nameOutofaHat.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python
 
 import random
+import sys
 
-# Enter names into the list called league.
-league = ['Name1', 'Name2', 'Name3', 'Name4', 'Name5', 'Name6']
+# Set league to the inputs and slice off the script name
+league = sys.argv[1:]
 
 # Function that takes in the list league and shuffles the list based on the number of iterations
 def pickNameOutofHat(iterations):
